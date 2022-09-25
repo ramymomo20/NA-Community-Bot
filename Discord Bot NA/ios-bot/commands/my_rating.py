c854042bffb85f4faa1229603fcbfa210c8ba721
Ramy Mohammed <ramyamohammed@outlook.com>
2022-09-24 23:46:16 -0400

    October Ratings
    new ratings with new features with them.

--- Discord Bot NA/ios-bot/commands/my_rating.py
+++ Discord Bot NA/ios-bot/commands/my_rating.py
@@ -14,16 +14,29 @@ async def my_rating(ctx):
         name1 = list(data.Name)
         pos = list(data.Position)
         rate = list(data.Rating)
-        ids = list(data.id)
+        ids = list(data.ID)
+        improved = list(data.Improved)
         x = [str(i) for i in ids]
 
 
-        dict1 = {x[a]:(pos[a],rate[a],name1[a]) for a in range(len(name1))}
+        dict1 = {x[a]:(pos[a],rate[a],name1[a],improved[a]) for a in range(len(name1))}
 
         if userid not in dict1:
             await ctx.respond(YOUR_ACCOUNT_NOT_FOUND,ephemeral=True)
         else:
             col = discord.Color.greyple()
+
+            if dict1[userid][3] == 100:
+                tex = 'You are a new addition!'
+            else:
+                if dict1[userid][3] < 0:
+                    num = int(dict1[userid][3])
+                    tex = f"```diff\n-{num*-1} from last rating.```"
+                if dict1[userid][3] == 0:
+                    tex = "```fix\n0 from last rating.```"
+                if dict1[userid][3] > 0:
+                    num = int(dict1[userid][3])
+                    tex = f"```diff\n+{num} from last rating.```"
 
             if dict1[userid][1] >= 84:
                 col = discord.Color.gold()
@@ -40,5 +53,6 @@ async def my_rating(ctx):
             embed.add_field(name="Player", value=dict1[userid][2], inline=True)
             embed.add_field(name="Position", value=dict1[userid][0], inline=True)
             embed.add_field(name="Rating", value=dict1[userid][1], inline=True)
+            embed.add_field(name="Change", value=tex,inline=False)
             embed.set_footer(text="Requested by {}".format(ctx.author.name))
             await ctx.respond(embed=embed)             
--- Discord Bot NA/ios-bot/commands/view_rating.py
+++ Discord Bot NA/ios-bot/commands/view_rating.py
@@ -13,16 +13,29 @@ async def view_rating(ctx, user: Option(discord.Member, "Enter an @", required =
         name1 = list(data.Name)
         pos = list(data.Position)
         rate = list(data.Rating)
-        ids = list(data.id)
+        ids = list(data.ID)
+        improved = list(data.Improved)
         x = [str(i) for i in ids]
 
 
-        dict1 = {x[a]:(pos[a],rate[a],name1[a]) for a in range(len(name1))}
+        dict1 = {x[a]:(pos[a],rate[a],name1[a], improved[a]) for a in range(len(name1))}
 
         if str(user.id) not in dict1:
             await ctx.respond(ACCOUNT_NOT_FOUND,ephemeral=True)
         else:
             col = discord.Color.greyple()
+
+            if dict1[userid][3] == 100:
+                tex = 'You are a new addition!'
+            else:
+                if dict1[userid][3] < 0:
+                    num = int(dict1[userid][3])
+                    tex = f"```diff\n-{num*-1} from last rating.```"
+                if dict1[userid][3] == 0:
+                    tex = "```fix\n0 from last rating.```"
+                if dict1[userid][3] > 0:
+                    num = int(dict1[userid][3])
+                    tex = f"```diff\n+{num} from last rating.```"
 
             if dict1[userid][1] >= 84:
                 col = discord.Color.gold()
@@ -39,5 +52,6 @@ async def view_rating(ctx, user: Option(discord.Member, "Enter an @", required =
             embed.add_field(name="Player", value=dict1[userid][2], inline=True)
             embed.add_field(name="Position", value=dict1[userid][0], inline=True)
             embed.add_field(name="Rating", value=dict1[userid][1], inline=True)
+            embed.add_field(name="Change", value=tex,inline=False)
             embed.set_footer(text="Requested by {}".format(ctx.author.name))
             await ctx.respond(embed=embed)
